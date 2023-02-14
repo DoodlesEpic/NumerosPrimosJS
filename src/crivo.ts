569923f41eaa9724f32264285eba6e37c08a87d4
Doodles <moraes.eduardo@proton.me>
2023-02-14 19:55:32 -0300

    test: Testes iniciais para crivoEratostenes

---
+++ src/crivo.ts
@@ -0,0 +1,32 @@
+export default function crivoEratostenes(n: number) {
+  // Array que armazena todos os números até o n
+  // O próximo passo será marcar os não primos nessa lista
+  let numeros: boolean[] = [];
+  for (let i = 0; i < n; i++) {
+    numeros.push(true);
+  }
+
+  // Marcamos os múltiplos de primos
+  // Todos números marcados como false NÃO são primos
+  const limite = Math.sqrt(n);
+  for (let i = 2; i <= limite; i++) {
+    if (numeros[i])
+      for (let j = i * i; j < n; j += i) {
+        numeros[j] = false;
+      }
+  }
+
+  // Todos itens setados como true são primos
+  // Criamos um novo array que apenas contém os números primos
+  let primos: number[] = [];
+  let primosAteNumero: number[] = [];
+  for (let i = 2; i < n; i++) {
+    // O número de primos até certo número é usado para o desenho do gráfico
+    primosAteNumero.push(primos.length);
+
+    // Apenas adicionamos ao resultado os números marcados primos
+    if (numeros[i]) primos.push(i);
+  }
+
+  return [primos, primosAteNumero];
+}
--- vite.config.ts
+++ vite.config.ts
@@ -18,4 +18,5 @@ export default {
     port: 8080,
     hot: true,
   },
+  test: {},
 };
